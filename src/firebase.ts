5fe6e23163c4b4d2e064f66616c27ffd8cfe3f53
Nacho Geoghegan <ignaciog00@gmail.com>
2023-07-26 22:47:59 -0300

    fixed missing database field on firebase config

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
+// import { getAnalytics } from 'firebase/analytics'; // ? not needed for now
 import { getFirestore } from 'firebase/firestore';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -11,6 +12,7 @@ const firebaseConfig = {
   apiKey: 'AIzaSyCIpD_OQTykmsufMS4N0XAkpzgXjdHiw5M',
   appId: '1:438482493030:web:7baa55bc12f4547ae457a7',
   authDomain: 'fir-test-1-394100.firebaseapp.com',
+  databaseURL: 'database.firebaseio.com',
   measurementId: 'G-0JP2S9P0EM',
   messagingSenderId: '438482493030',
   projectId: 'firebase-test-1-394100',
@@ -20,6 +22,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app); // ? not needed for now
 
 export const db = getFirestore(app);
